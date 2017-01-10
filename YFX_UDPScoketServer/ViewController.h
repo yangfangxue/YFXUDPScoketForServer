2626780b9ebfa1f8b502da69e6e82b505a57f3e7
xue-ios <122092669@qq.com>
2017-01-10 13:29:06 +0800

    Initial Commit

---
+++ YFX_UDPScoketServer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFX_UDPScoketServer
+//
+//  Created by fangxue on 2017/1/10.
+//  Copyright © 2017年 fangxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YFX_UDPScoketServer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YFX_UDPScoketServer
+//
+//  Created by fangxue on 2017/1/10.
+//  Copyright © 2017年 fangxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
